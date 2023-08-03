700d39a713e8ab499766562eccedd8d9b6265ccb
Stefan <stefanatanasovski04@gmail.com>
2023-08-03 12:45:58 +0200

    installed-pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PostModule } from './posts/post.module';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -15,6 +15,7 @@ import { PostModule } from './posts/post.module';
   imports: [
     BrowserModule,
     HttpClientModule,
+    NgxPaginationModule,
     PostModule,
     RouterModule.forRoot([
       {path:'', redirectTo:'posts',pathMatch:'full'},
